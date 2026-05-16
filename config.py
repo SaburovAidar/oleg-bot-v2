f5756a6bf4ec7837b820e869ca082d8b0a75c395
SaburovAidar <SaburovAidar1312@gmail.com>
2026-05-16 15:02:28 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BOT_TOKEN       = os.environ.get("BOT_TOKEN", "8633695431:AAGQNi9mh0tK_XCZ44eUNU3Ajw_PBK9QtrU")
+APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbye1M-P0gLaUdqkhv77YaEvJhN3EzqB9GHQTyOLNyhl_owL5ivC0APr7WEdIfn0mdLM/exec"
+
+CHARACTER_NAME        = "Олег Сергеевич | @OlegSergeevichGibdd"
+CHARACTER_DESCRIPTION = (
+    "🔰 Помощь с авто-документами\n"
+    "🚗 Водительские удостоверения\n"
+    "📑 СТС • ПТС • Документы автошколы\n"
+    "🏥 Медицинские справки\n"
+    "⚡ Оперативно и надёжно"
+)
+CHARACTER_IMAGE_URL = "https://i.postimg.cc/QM9p9mhJ/111.jpg"
